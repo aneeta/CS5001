d8fe91dc902bf11eaf13d5d874ee84fff3577dd3
Aneta <anette812s@gmail.com>
2022-10-28 05:55:34 +0100

    classpath config

---
+++ CS5001-p2-stratego/env_setup.sh
@@ -0,0 +1,4 @@
+export JUNIT_INSTALL_PATH=/home/as627/workspace_linux/usr/junit-4.13.2.jar
+export HAMCREST_INSTALL_PATH=/home/as627/workspace_linux/usr/hamcrest-core-1.3.jar
+export STRATEGO_PATH=/home/as627/workspace_linux/sem_1/CS5001/CS5001-p2-stratego/src/stratego
+export CLASSPATH="$CLASSPATH:$JUNIT_INSTALL_PATH:$HAMCREST_INSTALL_PATH:$STRATEGO_PATH"
