d1df534a3dcf3787938d4ebf5a9f5700e8ab7d8f
aneeta <as627@pc7-018-l.cs.st-andrews.ac.uk>
2022-11-14 03:09:58 +0000

    README

--- CS5001-p3-chatserver/src/ircserver/Channel.java
+++ CS5001-p3-chatserver/src/ircserver/Channel.java
@@ -4,31 +4,54 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class representing a single channel on the chat server.
+ */
 public class Channel {
     private String name;
     private List<User> users;
 
+    /**
+     * Constructor for Channel.
+     * @param name channel name
+     */
     public Channel(String name) {
         this.name = name;
         users = new ArrayList<>();
     }
 
+    /** Getter method for Channel name.
+     * @return String
+     */
     public String getName() {
         return name;
     }
 
+    /** Method to add user tp the Channel.
+     * @param user User
+     */
     public void addUser(User user) {
         users.add(user);
     }
 
+    /** Method to remove user from the Channel.
+     * @param user User
+     */
     public void removeUser(User user) {
         users.remove(user);
     }
 
+    /** Method to check if the Channel is empty.
+     * @return Boolean
+     */
     public Boolean hasUsers() {
         return (users.size() > 0);
     }
 
+    /** Method writing a message to all users in the Channel.
+     * @param msg message to write
+     * @throws IOException
+     */
     public void message(String msg) throws IOException {
         for (User u : users) {
             ChatServer.write(u, msg);
--- CS5001-p3-chatserver/src/ircserver/exceptions/DisconnectedException.java
+++ CS5001-p3-chatserver/src/ircserver/exceptions/DisconnectedException.java
@@ -1,7 +1,13 @@
 package ircserver.exceptions;
 
+/**
+ * Class for the DisconnectedException.
+ */
 public class DisconnectedException extends Exception {
-
+    /**
+     * Custom Disconnected client exception.
+     * @param message
+     */
     public DisconnectedException(String message) {
         super(message);
     }
